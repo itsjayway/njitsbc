bfd97b1abcab4b98d3a5d01976f54ce03a3749dd
itsjayway <jibrana.ja@proton.me>
2025-10-12 00:22:48 -0400

    more consistent thumbnail loading

--- backend/listClips/__init__.py
+++ backend/listClips/__init__.py
@@ -1,6 +1,11 @@
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
 import json
 import logging
 import os
+import requests
 
 from azure.core.credentials import AzureNamedKeyCredential
 from azure.data.tables import TableClient
@@ -14,24 +19,68 @@ ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
 ACCOUNT_KEY = os.getenv("AZURE_STORAGE_ACCOUNT_KEY")
 TABLE_NAME = os.getenv("AZURE_STORAGE_TABLE_NAME")
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         credential = AzureNamedKeyCredential(ACCOUNT_NAME, ACCOUNT_KEY)
-        
+
         table_client = TableClient(
             endpoint=f"https://{ACCOUNT_NAME}.table.core.windows.net",
             table_name=TABLE_NAME,
-            credential=credential
+            credential=credential,
         )
 
         entities = table_client.list_entities(filter="approved eq true")
-        clips = sorted([entity for entity in entities if entity.get("approved") is True], key=lambda x: x['date'], reverse=True)
+        clips = sorted(
+            [entity for entity in entities if entity.get("approved") is True],
+            key=lambda x: x["date"],
+            reverse=True,
+        )
+        try:
+            for clip in clips:
+                blob_url = clip["blobUrl"]
+                if is_video_file(blob_url):
+                    clip["thumbnail"] = get_nth_frame(
+                        blob_url, -1
+                    )  # TODO: get frame # from metadata
+                else:
+                    clip["thumbnail"] = get_image_thumbnail(blob_url)
+        except Exception as e:
+            logging.error(f"Error generating thumbnail: {e}", exc_info=True)
 
         return func.HttpResponse(
-            json.dumps(clips),
-            mimetype="application/json",
-            status_code=200
+            json.dumps(clips), mimetype="application/json", status_code=200
         )
     except Exception as e:
-        logging.error(f"Error listing clips: {e}", exc_info=True)
-        return func.HttpResponse(str(e), status_code=500)
+        return func.HttpResponse(json.dumps({"error": str(e)}), status_code=500)
+
+
+def is_video_file(filename: str) -> bool:
+    filename = filename.lower()
+    video_extensions = [".mp4", ".mov", ".avi", ".mkv"]
+    return any(filename.endswith(ext) for ext in video_extensions)
+
+
+def get_nth_frame(video_url: str, n: int) -> str:
+    cap = cv2.VideoCapture(video_url)
+    if n == -1:
+        p = .25 # percent progress in the vid
+        n = int(cap.get(cv2.CAP_PROP_FRAME_COUNT) * p)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, n)
+    ret, frame = cap.read()
+    cap.release()
+    if not ret:
+        return ""
+    _, buffer = cv2.imencode(".jpg", frame)
+    return "data:image/jpeg;base64," + base64.b64encode(buffer).decode("utf-8")
+
+
+def get_image_thumbnail(image_url: str) -> str:
+    resp = requests.get(image_url)
+    image = Image.open(BytesIO(resp.content))
+    image.thumbnail((512, 512))
+    buffer = BytesIO()
+    image.save(buffer, format="JPEG")
+    return "data:image/jpeg;base64," + base64.b64encode(buffer.getvalue()).decode(
+        "utf-8"
+    )
--- frontend/src/components/Clip/ClipThumbnail.tsx
+++ frontend/src/components/Clip/ClipThumbnail.tsx
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { memo } from "react";
 import type Clip from "../../interfaces/ClipInterface";
 import validateFileExt from "../../utils/validateFileExt";
-import { useMsal } from "@azure/msal-react";
 import classes from "../../utils/classes";
+import { useUser } from "../../hooks/useUser";
 
 interface ClipThumbnailProps {
   onClick: () => void;
   clip: Clip;
 }
 
-export default function ClipThumbnail({ onClick, clip }: ClipThumbnailProps) {
-  const { instance } = useMsal();
-  const username = instance.getActiveAccount()?.name;
+export default memo(function ClipThumbnail({ onClick, clip }: ClipThumbnailProps) {
+  const { user } = useUser();
+  const username = user?.displayName;
 
   const fileNameLowercase = clip.fileName.toLowerCase();
   const clipIsVideo = validateFileExt(fileNameLowercase, ["mp4", "mov", "webm"]);
   const clipIsImage = validateFileExt(fileNameLowercase, ["jpg", "jpeg", "png"]);
+
   return (
     <div key={clip.RowKey} className="cursor-pointer" onClick={() => onClick()}>
       <div className="relative">
@@ -33,23 +34,31 @@ export default function ClipThumbnail({ onClick, clip }: ClipThumbnailProps) {
             @{clip.uploadedBy}
           </span>
         </div>
-        {clipIsImage && (
+
+        {/* media type chip */}
+        <div className="hidden sm:flex absolute bottom-3 right-3">
+          <span
+            className={classes(
+              "bg-opacity-75 px-2 py-1 rounded-lg text-sm",
+              "bg-gray-400 text-gray-800 font-light"
+            )}
+          >
+            {clipIsVideo ? "Video" : "Image"}
+          </span>
+        </div>
+
+
+        {(clipIsImage || clipIsVideo) && clip.thumbnail && (
           <img
-            src={clip.blobUrl}
-            className="object-cover rounded-2xl aspect-square w-full"
-            alt={clip.description || "Clip image"}
+            src={clip.thumbnail}
+            className={classes(
+              "object-cover rounded-2xl aspect-square w-full transition-opacity duration-300",
+            )}
+            alt={clip.description || "Clip thumbnail"}
             loading="lazy"
-          />
-        )}
-        {clipIsVideo && (
-          <video
-            src={clip.blobUrl}
-            className="object-cover rounded-2xl aspect-square w-full"
-            muted
-            preload="metadata"
           />
         )}
       </div>
     </div>
   );
-}
+});
